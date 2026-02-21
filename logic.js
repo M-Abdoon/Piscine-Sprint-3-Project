1946350e78554ffdbd5dcb90b220971c571f9602
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2026-02-21 18:41:52 +0000

    error when no user should be recognised

--- logic.js
+++ logic.js
@@ -3,12 +3,13 @@ export async function getUserInfo(username) {
     const url = await fetch(
       `https://www.codewars.com/api/v1/users/${username}`,
     );
-    if (!url.ok) {
-      return false;
+    if (url.status === 404) {
+      return { notFound: true, username };
     }
+
     return await url.json();
   } catch (error) {
-    return false;
+    return { error: true, message: err.message };
   }
 }
 
@@ -19,8 +20,14 @@ export async function fetchAllUsersData(listOfUsernames) {
   for (const username of allUsersInArray) {
     const userData = await getUserInfo(username);
 
-    if (!userData) {
+    if (userData.notFound) {
       allUsersFetchedData.push({ [username]: null });
+      msgToUser.innerHTML += `User not found: ${username}<br>`;
+      continue;
+    }
+
+    if (userData.error) {
+      msgToUser.innerHTML += `Error when fetching ${username}: ${result.message}<br>`;
       continue;
     }
 
